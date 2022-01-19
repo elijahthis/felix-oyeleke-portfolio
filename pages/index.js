17602aff0bd587431be29c2e1865a6431209a2e7
elijahthis <emperorelijah007@gmail.com>
2022-01-19 03:49:19 +0100

    meta tags

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,23 @@ export default function Home() {
     <>
       <Head>
         <title>Felix Oyeleke - Portfolio</title>
+        <meta property="og:title" content="Felix Oyeleke - Portfolio" />
+        <meta property="og:url" content="https://felix-oyeleke.netlify.app/" />
+        <meta
+          property="og:image"
+          content="https://felix-oyeleke.netlify.app/icon.png"
+        />
+        <meta property="og:type" content="profile" />
+        <meta
+          property="og:description"
+          content="My name is Felix Oyeleke. I am a Hydrographic Surveyor. I can also be a Marine Geophysicist, Data Scientist and Computer Vision Enthusiast as the situation demands. Welcome to my portfolio website. Enjoy..."
+        />
+        <meta property="og:locale" content="en_US" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@fsoyeleke" />
+        <meta name="twitter:creator" content="@elijahthis" />
+        <meta name="description" content="Felix Oyeleke's portfolio website" />
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/icon.png" />
       </Head>
       <div id="container">
         <Hero />
